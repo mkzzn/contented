efc16d8cceb74977d5d2c246802f9e9af80a1619
Mike Zazaian <mk@mkzzn.org>
2011-01-23 18:55:29 -0500

    get rid of extraneous article route declarations

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,6 @@ Contented::Application.routes.draw do
   get "categories/new"
 
   get "categories/edit"
-
-  get "articles/index"
-
-  get "articles/view"
-
-  get "articles/edit"
-
-  get "articles/new"
-
-  post "articles/create"
   resources :articles
 
   root :to => "home#index"
