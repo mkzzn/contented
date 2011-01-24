3050b04d3af24542e1de8b32d0c2f6545d006a14
Mike Zazaian <mk@mkzzn.org>
2011-01-23 19:33:36 -0500

    add feature/steps for article creation

---
+++ features/articles/create.feature
@@ -0,0 +1,19 @@
+Feature: Create Article
+  In order to publish content
+  As a Snakewise Writer
+  I want to be able to create articles
+  
+  Scenario: User Successfully Creates an Article
+    Given I am on the new article page
+    And I enter valid data for an article entitled "goats"
+    When I submit the article
+    Then I should be on the articles index page
+    And I should see article "goats"
+    And I should see a notice saying that article "goats" was created
+ 
+  Scenario: User Fails to create an Article
+    Given I am on the new article page
+    And I enter insufficient values for an article
+    When I submit the article
+    Then I should be on the new article page
+    And I should see a warning saying that my article was not created
---
+++ features/step_definitions/articles/creation_steps.rb
@@ -0,0 +1,24 @@
+Given /^I enter valid data for an article entitled "([^"]*)"$/ do |title|
+  fill_in "article_title", :with => title
+  fill_in "article_body", :with => "Something Else"
+end
+
+When /^I submit the article$/ do
+  find(:css, "#article_submit").click
+end
+
+Then /^I should see article "([^"]*)"$/ do |title|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see a notice saying that article "([^"]*)" was created$/ do |title|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I enter insufficient values for an article$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see a warning saying that my article was not created$/ do
+  pending # express the regexp above with the code you wish you had
+end
