48a6c233caffc8e41a62fc4b63a0ac3ee162a875
Mike Zazaian <mk@mkzzn.org>
2011-01-24 22:03:46 -0500

    simplify flash helper, get it working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,7 @@
 module ApplicationHelper
-  def show_flash(options={})
-    # set the default tag to a <div> element
-    opts = {:tag => :div, :additional_attributes => {}}.merge options
-    
-    # create a div for each available flash type and display its value
-    [:notice, :warning, :message, :failure, :success].collect do |key|
-      next if flash[key].blank?
-      attributes = {:class => "flash flash_#{key} #{key}"}.merge opts[:additional_attributes]
-      content_tag(opts[:tag], flash[key], attributes)
+  def show_flash
+    [:notice, :warning].collect do |key|
+      content_tag :div, flash[key], {:class => "flash #{key}"}
     end.join
   end
 end
