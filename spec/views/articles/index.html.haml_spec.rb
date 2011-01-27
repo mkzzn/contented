c4396e26752e421e5ff9dc6dd04d51420cbb8d0b
Mike Zazaian <mk@mkzzn.org>
2011-01-27 01:25:47 -0500

    add basic article partial rendering spec, add New Article page title

--- spec/views/articles/index.html.haml_spec.rb
+++ spec/views/articles/index.html.haml_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe "articles/index.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should render the article partial for each article" do
+    assign(:articles, [stub_model(Article), stub_model(Article)])
+    render
+    view.should render_template(:partial => "_article", :count => 2)
+  end
 end
