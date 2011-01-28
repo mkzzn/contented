d5180ee1de79cfb2fa43a903bb64eb859db4c890
Mike Zazaian <mk@mkzzn.org>
2011-01-27 21:49:57 -0500

    pass form-related article view specs

--- spec/views/articles/edit.html.haml_spec.rb
+++ spec/views/articles/edit.html.haml_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "articles/edit.html.haml" do
   it "should render the form" do
+    assign :article, stub_model(Article)
+    render
     view.should render_template("_form", :count => 1)
   end
 end
