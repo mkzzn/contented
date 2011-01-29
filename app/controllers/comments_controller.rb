a08d6286ee755d47bea603b70303ec2fa723699a
Mike Zazaian <mk@mkzzn.org>
2011-01-29 14:08:58 -0500

    get the comment specs to pass

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,5 +2,11 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find params[:article_id]
     @comment = @article.create_comment params[:comment]
+
+    if @comment.valid?
+      redirect_to @article
+    else
+      render "articles/show"
+    end
   end
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,30 +1,34 @@
 require 'spec_helper'
 
+def create_params(comments_hash)
+  { "article_id" => "3" }.merge comments_hash
+end
+
+def create_setup(comment_body)
+  @comments_hash = {"comment" => {"body" => comment_body}}
+  @comment = mock_model(Comment, :body => comment_body)
+  @article.should_receive(:create_comment).with(@comments_hash["comment"]).and_return @comment
+end
+
 describe CommentsController do
   context "POST 'create'" do
     before(:each) do
-      @comment_hash = { "body" => "kwl!" }
-      @params = { "article_id" => "3", "comment" => @comment_hash }
-      article = mock_model(Article, :id => 3)
-      comment = mock_model(Comment, :body => "kwl!")
-
-    end
-
-    it "should find the parent article" do
-      Article.should_receive(:find).with("3").and_return article
+      @article = mock_model(Article, :id => "3")
+      Article.should_receive(:find).with("3").and_return @article
     end
 
     it "should redirect to the article on success" do
+      create_setup("kwl!")
+      @comment.stub!(:valid?).and_return true
+      post 'create', create_params(@comments_hash)
+      response.should redirect_to(@article)
     end
 
     it "should render the view article action on failure" do
-      
-    end
-
-    it "should find the parent article" do
-      article.should_receive(:create_comment).with(@comment_hash).and_return comment
-      post 'create', @params
-      response.should render_nothing
+      create_setup("")
+      @comment.stub!(:valid?).and_return false
+      post 'create', create_params(@comments_hash)
+      response.should render_template("show")
     end
   end
 end
