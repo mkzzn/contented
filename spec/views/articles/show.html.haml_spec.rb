b440d310ead6c715cd3c0900bfdd9636cdf2ac60
Mike Zazaian <mk@mkzzn.org>
2011-01-29 15:02:31 -0500

    render article comments

--- spec/views/articles/show.html.haml_spec.rb
+++ spec/views/articles/show.html.haml_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe "articles/show.html.haml" do
+  it "should render a partial for each comment" do
+    article = mock_model(Article)
+    article.should_receive(:title)
+    article.should_receive(:body)
+    article.stub!(:comments).and_return [stub_model(Comment), stub_model(Comment)]
+    assign(:article, article)
+    render
+    view.should render_template(:partial => "comments/_comment", :count => 2)
+  end
 end
