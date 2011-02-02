d9add229ab4ea04a48a1c454dbae48bfe361cc87
Mike Zazaian <mk@mkzzn.org>
2011-02-01 23:54:04 -0500

    pass some validation specs for Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ActiveRecord::Base
-  validate :title, :presence => true
+  validates :title, :presence => true
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,14 +2,26 @@ require 'spec_helper'
 
 describe Category do
   context "validations" do
-    context "attributes" do
-      before(:each) do
-        @category = Factory.build :category, :title => nil, :body => nil 
+    context "pass" do
+      it "should validate a legit title" do
+        @category = Factory.build :category, :title => "walruses"
         @category.save
+        @category.should be_valid
+      end
+    end
+
+    context "fail" do
+      it "should not validate a nil title" do
+        @category = Factory.build :category, :title => nil
       end
 
-      it "should validate presence of title" do
-        @article.should_not be_valid
+      it "should not validate an empty title" do
+        @category = Factory.build :category, :title => ""
+      end
+      
+      after(:each) do
+        @category.save
+        @category.should_not be_valid
       end
     end
   end
