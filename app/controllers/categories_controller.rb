6c5829b0eab31a09afce7a3927e092ce9d66933e
Mike Zazaian <mk@mkzzn.org>
2011-02-01 23:22:10 -0500

    write/pass some POST create controller specs for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,6 +6,16 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
+  def create
+    @category = Category.create params[:category]
+    
+    if @category.valid?
+      redirect_to categories_path
+    else
+      render :action => "new"
+    end
+  end
+
   def edit
   end
 
--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -20,4 +20,24 @@ describe CategoriesController do
       get "new"
     end
   end
+
+  context "POST create" do
+    before(:each) do
+      @params = {"description" => "douglass", "title" => "fredrick"}
+      @category = mock_category(@params)
+      Category.should_receive(:create).with(@params) { @category }
+    end
+    
+    it "should redirect to the categories index on success" do
+      @category.stub!(:valid?) { true }
+      post "create", :category => @params
+      response.should redirect_to(categories_path)
+    end
+
+    it "should render the new action on failure" do
+      @category.stub!(:valid?) { false }
+      post "create", :category => @params
+      response.should render_template("new")
+    end
+  end
 end
