f064167f749118411d2cb2bf7105028fb7e4a7e7
Mike Zazaian <mk@mkzzn.org>
2011-02-06 18:41:41 -0500

    break article _form specific logic into separate spec

---
+++ spec/views/articles/_form.html.haml_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "articles/_form.html.haml" do
+  before(:each) do
+    assign :article, stub_model(Article)
+  end
+
+  it "should get all categories" do
+    categories = [mock_category, mock_category]
+    Category.should_receive(:all) { categories }
+    render
+  end
+
+  def mock_category(stubs={})
+    mock_model(Category).as_null_object.tap do |category|
+      category.stub(stubs) unless stubs.empty?
+    end
+  end
+end
--- spec/views/articles/edit.html.haml_spec.rb
+++ spec/views/articles/edit.html.haml_spec.rb
@@ -5,20 +5,8 @@ describe "articles/edit.html.haml" do
     assign :article, stub_model(Article)
   end
 
-  it "should get all categories" do
-    categories = [mock_category, mock_category]
-    Category.should_receive(:all) { categories }
-    render
-  end
-
   it "should render the form" do
     render
     view.should render_template("_form", :count => 1)
-  end
-
-  def mock_category(stubs={})
-    mock_model(Category).as_null_object.tap do |category|
-      category.stub(stubs) unless stubs.empty?
-    end
   end
 end
