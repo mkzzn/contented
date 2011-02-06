aa63c5ea1b25f9d26071ed2e592940a832b704ad
Mike Zazaian <mk@mkzzn.org>
2011-02-06 18:38:58 -0500

    improve edit article view spec to expect Category.all

--- features/articles/edit.feature
+++ features/articles/edit.feature
@@ -18,6 +18,7 @@ Feature: Edit Article
 
     Scenario: User submits invalid data
       Given I clear the article title
+      Then show me the page
       When I submit the changes
       And I should be shown that the title has an error
 
--- spec/views/articles/edit.html.haml_spec.rb
+++ spec/views/articles/edit.html.haml_spec.rb
@@ -1,9 +1,24 @@
 require 'spec_helper'
 
 describe "articles/edit.html.haml" do
-  it "should render the form" do
+  before(:each) do
     assign :article, stub_model(Article)
+  end
+
+  it "should get all categories" do
+    categories = [mock_category, mock_category]
+    Category.should_receive(:all) { categories }
+    render
+  end
+
+  it "should render the form" do
     render
     view.should render_template("_form", :count => 1)
+  end
+
+  def mock_category(stubs={})
+    mock_model(Category).as_null_object.tap do |category|
+      category.stub(stubs) unless stubs.empty?
+    end
   end
 end
