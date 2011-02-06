f474e27420fb4e3957d486c11036d1702bbe02ad
Mike Zazaian <mk@mkzzn.org>
2011-02-06 18:55:42 -0500

    fix bugs due to use of @categories in sidebar partial

--- spec/views/nav/_primary.html.haml_spec.rb
+++ spec/views/nav/_primary.html.haml_spec.rb
@@ -1,9 +1,25 @@
 require 'spec_helper'
 
 describe "/nav/_primary.html.haml" do
+
+  before(:each) do
+    @categories = [ mock_category, mock_category ]
+  end
+
+  it "should get all categories" do
+    Category.should_receive(:all) { @categories }
+    render
+  end
+
   it "should render the nav category partial for each category" do
-    assign :categories, [stub_model(Category), stub_model(Category)]
+    Category.stub!(:all) { @categories }
     render
     view.should render_template(:partial => "_category", :count => 2)
+  end
+
+  def mock_category(stubs={})
+    mock_model(Category).as_null_object.tap do |category|
+      category.stub(stubs) unless stubs.empty?
+    end
   end
 end
