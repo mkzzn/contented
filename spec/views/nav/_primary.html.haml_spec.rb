e565e4af14fddb780303e06aac803e0e39f5d4ae
Mike Zazaian <mk@mkzzn.org>
2011-02-11 22:04:15 -0500

    working on view specs for 'uncategorized' link in categories sidebar

--- spec/views/nav/_primary.html.haml_spec.rb
+++ spec/views/nav/_primary.html.haml_spec.rb
@@ -17,6 +17,20 @@ describe "/nav/_primary.html.haml" do
     view.should render_template(:partial => "_category", :count => 2)
   end
 
+  context "uncategorized link" do
+    it "should have one if an uncategorized article exists" do
+      Article.stub_chain(:uncategorized, :count) { 3 }
+      render
+      rendered.should =~ "<li class='category'><a href=\"/articles/uncategorized\">Uncategorized (3)</a></li>"
+    end
+
+    it "should not have one if no uncategorized articles exists" do
+      Article.stub_chain(:uncategorized, :count) { 0 }
+      render
+      rendered.should_not =~ /Uncategorized/
+    end
+  end
+
   def mock_category(stubs={})
     mock_model(Category).as_null_object.tap do |category|
       category.stub(stubs) unless stubs.empty?
