92f00aaf741b8d2929c8abb18e394101dd6518c4
Mike Zazaian <mk@mkzzn.org>
2011-03-08 10:47:07 -0500

    add inverse-case specs for primary nav link activation

--- spec/views/navigation/_primary.html.haml_spec.rb
+++ spec/views/navigation/_primary.html.haml_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe '/nav/_primary.html.haml' do
+
   context "pages controller" do
     context "homepage action" do
-      it "should activate the home link" do
+      it "should activate the 'Home' link" do
         controller.stub!(:controller_name) { "pages" }
         controller.stub!(:action_name) { "homepage" }
         render
@@ -11,23 +12,51 @@ describe '/nav/_primary.html.haml' do
         rendered.should include(homepage_link)
       end
     end
+    context "non-homepage action" do
+      it "should not activate the 'Home' link" do
+        controller.stub!(:controller_name) { "pages" }
+        controller.stub!(:controller_name) { "puck man" }
+        render
+        homepage_link = link_to "Home", root_path, :class => "home title"
+        rendered.should include(homepage_link)
+      end
+    end
   end
 
   context "articles controller" do
-    it "should activate the articles link" do
-      controller.stub!(:controller_name) { "articles" }
-      render
-      articles_link = link_to "Articles", articles_path, :class => "articles title active"
-      rendered.should include(articles_link)
+    context "active" do
+      it "should activate the articles link" do
+        controller.stub!(:controller_name) { "articles" }
+        render
+        articles_link = link_to "Articles", articles_path, :class => "articles title active"
+        rendered.should include(articles_link)
+      end
+    end
+    context "inactive" do
+      it "should not activate the articles link" do
+        render
+        articles_link = link_to "Articles", articles_path, :class => "articles title"
+        rendered.should include(articles_link)
+      end
     end
   end
 
   context "categories controller" do
-    it "should activate the categories link" do
-      controller.stub!(:controller_name) { "categories" }
-      render
-      categories_link = link_to "Categories", categories_path, :class => "categories title active"
-      rendered.should include(categories_link)
+    context "active" do
+      it "should activate the categories link" do
+        controller.stub!(:controller_name) { "categories" }
+        render
+        categories_link = link_to "Categories", categories_path, :class => "categories title active"
+        rendered.should include(categories_link)
+      end
+    end
+
+    context "inactive" do
+      it "should not activate the categories link" do
+        render
+        categories_link = link_to "Categories", categories_path, :class => "categories title"
+        rendered.should include(categories_link)
+      end
     end
   end
 end
