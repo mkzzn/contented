99e3be591bd0c6597f6031d87e16e543fb78e42e
Mike Zazaian <mk@mkzzn.org>
2011-04-23 19:18:59 -0400

    add preliminary user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110129181145) do
+ActiveRecord::Schema.define(:version => 20110423225417) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -35,6 +35,27 @@ ActiveRecord::Schema.define(:version => 20110129181145) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "article_id"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "login",                              :null => false
+    t.string   "email",                              :null => false
+    t.string   "first_name",                         :null => false
+    t.string   "last_name",                          :null => false
+    t.string   "crypted_password",                   :null => false
+    t.string   "password_salt",                      :null => false
+    t.string   "persistence_token",                  :null => false
+    t.string   "single_access_token",                :null => false
+    t.string   "perishable_token",                   :null => false
+    t.integer  "login_count",         :default => 0, :null => false
+    t.integer  "failed_login_count",  :default => 0, :null => false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
