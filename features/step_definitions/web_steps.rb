cba527eb00e4c678e3a8d3b1cf83fd722e661877
Mike Zazaian <mk@mkzzn.org>
2011-04-23 15:33:54 -0400

    comment out new 'within' web steps generated by cucumber-rails that were fubaring 'within' scenarios

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -31,15 +31,15 @@ module WithinHelpers
 end
 World(WithinHelpers)
 
-# Single-line step scoper
-When /^(.*) within ([^:]+)$/ do |step, parent|
-  with_scope(parent) { When step }
-end
+# # Single-line step scoper
+# When /^(.*) within ([^:]+)$/ do |step, parent|
+#   with_scope(parent) { When step }
+# end
 
-# Multi-line step scoper
-When /^(.*) within ([^:]+):$/ do |step, parent, table_or_string|
-  with_scope(parent) { When "#{step}:", table_or_string }
-end
+# # Multi-line step scoper
+# When /^(.*) within ([^:]+):$/ do |step, parent, table_or_string|
+#   with_scope(parent) { When "#{step}:", table_or_string }
+# end
 
 Given /^(?:|I )am on (.+)$/ do |page_name|
   visit path_to(page_name)
