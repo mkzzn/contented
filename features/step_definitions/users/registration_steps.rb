6464f8b03fcb5ef51c19a56eb560b45df32e860f
Mike Zazaian <mk@mkzzn.org>
2011-04-26 10:51:41 -0400

    implement initial registration view for new users

--- features/step_definitions/users/registration_steps.rb
+++ features/step_definitions/users/registration_steps.rb
@@ -1,15 +1,15 @@
 Given /^I enter all valid data for new user "([^"]*)" with password "([^"]*)"$/ do |login, password|
   user = Factory.attributes_for(:user)
   fill_in "user_login", :with => login
-  fill_in "user_email", :with => user.email
-  fill_in "user_first_name", :with => user.first_name
-  fill_in "user_last_name", :with => user.last_name
+  fill_in "user_email", :with => user[:email]
+  fill_in "user_first_name", :with => user[:first_name]
+  fill_in "user_last_name", :with => user[:last_name]
   fill_in "user_password", :with => password
   fill_in "user_password_confirmation", :with => password
 end
 
 When /^I submit the registration form$/ do
-  pending # express the regexp above with the code you wish you had
+  find(:css, "#user_submit").click
 end
 
 Given /^I fail to enter all valid data for user registration$/ do
