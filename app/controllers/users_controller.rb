ba15ff670db0b349393c221ab3b10a5962c1142d
Mike Zazaian <mk@mkzzn.org>
2011-04-26 12:27:55 -0400

    building out users controller, 'create' action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.create params[:user]
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,4 +13,45 @@ describe UsersController do
       get "new"
     end
   end
+
+  context "POST 'create'" do
+
+    context "success" do
+      before(:each) do
+        @user_attrs = Factory.attributes_for(:user).inject({}) do |hash, pair|
+          hash[pair[0].to_s] = pair[1]
+          hash
+        end
+        @user = mock_user(@user_attrs)
+      end
+
+      it "should create a new user" do
+        User.should_receive(:new).with(@user_attrs) { @user }
+        post "create", :user => @user_attrs
+      end
+
+      it "should redirect to the articles path" do
+        post "create", :user => @user_attrs
+        response.code.should == "302"
+        response.should redirect_to(user_confirmation_path)
+      end
+    end
+
+    context "failure" do
+      before(:each) do
+        @mock_user = mock_user({})
+      end
+
+      it "should create a new user" do
+        User.should_receive(:new).with({}) { mock_user }
+        post "create", :user => {}
+      end
+
+      it "should redirect to the articles path" do
+        post "create", :user => {}
+        response.should_not redirect_to(user_confirmation_path)
+        response.code.should == "200"
+      end
+    end
+  end
 end
