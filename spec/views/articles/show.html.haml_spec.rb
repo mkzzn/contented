38369e2e046ffa7df89f0c8bb95d114816f9f74e
Mike Zazaian <mk@mkzzn.org>
2011-06-19 17:40:34 -0400

    cleaning up show article view spec

--- spec/views/articles/show.html.haml_spec.rb
+++ spec/views/articles/show.html.haml_spec.rb
@@ -14,85 +14,88 @@ describe "articles/show.html.haml" do
     render
   end
 
-  before(:each) do
-    @article_stubs = {
-      :category => stub_model(Category),
-      :categorized? => true,
-      :category_name => "ultracat"
-    }
-  end
+  # before(:each) do
+  #   @article_stubs = {
+  #     :category => stub_model(Category),
+  #     :categorized? => true,
+  #     :category_name => "ultracat"
+  #   }
+  # end
 
-  it "should render a partial for each comment" do
-    @article = mock_model(Article, @article_stubs)
-    expect_and_render_resources
-    view.should render_template(:partial => "comments/_comment", :count => 2)
-    rendered.should =~ /ultracat/
-  end
+  # it "should render a partial for each comment" do
+  #   @article = mock_model(Article, @article_stubs)
+  #   expect_and_render_resources
+  #   view.should render_template(:partial => "comments/_comment", :count => 2)
+  #   rendered.should =~ /ultracat/
+  # end
 
-  describe "delete link" do
-    before(:each) do
-      stub_current_ability
-      @article = mock_model(Article, @article_stubs)
-    end
+  # describe "delete link" do
+  #   before(:each) do
+  #     stub_current_ability
+  #     @article = mock_model(Article, @article_stubs)
+  #   end
 
-    context "user can delete article" do
-      it "should render a link to destroy the article" do
-        @ability.can :destroy, @article
-        expect_and_render_resources
-        rendered.should have_selector(".article input.destroy")
-      end
-    end
+  #   context "user can delete article" do
+  #     it "should render a link to destroy the article" do
+  #       @ability.can :destroy, @article
+  #       expect_and_render_resources
+  #       rendered.should have_selector(".article input.destroy")
+  #     end
+  #   end
     
-    context "user cannot delete article" do
-      it "should not render a link to destroy the article" do
-        @ability.cannot :destroy, @article
-        expect_and_render_resources
-        rendered.should_not have_selector(".article input.destroy")
-      end
-    end
+  #   context "user cannot delete article" do
+  #     it "should not render a link to destroy the article" do
+  #       @ability.cannot :destroy, @article
+  #       expect_and_render_resources
+  #       rendered.should_not have_selector(".article input.destroy")
+  #     end
+  #   end
+  # end
+
+  before(:each) do
+    @article = stub_article
+    assign :article, @article
+    assign :comments, [ stub_comment, stub_comment ] 
+    assign :comment, stub_comment
   end
 
   describe "edit link" do
     before(:each) do
       stub_current_ability
-      @article = mock_model(Article, @article_stubs)
     end
 
     context "user can edit article" do
       it "should render a link to edit the article" do
-        @ability.can :edit, @article
-        expect_and_render_resources
+        @ability.can :edit, Article
+        render
         rendered.should have_selector(".article .resource_links a.edit")
       end
     end
     
     context "user cannot edit article" do
       it "should not render a link to edit the article" do
-        @ability.cannot :edit, @article
-        expect_and_render_resources
+        @ability.cannot :edit, Article
+        render
         rendered.should_not have_selector(".article .resource_links a.edit")
       end
     end
   end
 
   describe "category link" do
-    context "article is uncategorized" do
+    context "article is not categorized" do
       it "should not render the category link" do
-        @article = mock_model Article, @article_stubs.merge(:categorized? => false)
-        expect_and_render_resources
-        view.should render_template(:partial => "comments/_comment", :count => 2)
-        rendered.should_not =~ /ultracat/
-        rendered.should =~ /Uncategorized/
+        @article.stub!(:categorized?) { false }
+        render
+        rendered.should include("Uncategorized")
       end
     end
 
     context "article is categorized" do
       it "should render the category link" do
-        @article = mock_model Article, @article_stubs
-        expect_and_render_resources
-        view.should render_template(:partial => "comments/_comment", :count => 2)
-        rendered.should =~ /ultracat/
-        rendered.should_not =~ /Uncategorized/
+        @category = mock_category :id => 10, :title => "Snake Bait"
+        @article.stub!(:category) { @category }
+        render
+        rendered.should include("Snake Bait")
       end
     end
   end
@@ -102,15 +105,15 @@ describe "articles/show.html.haml" do
       category.stub(stubs) unless stubs.empty?
     end
   end
-
-  def mock_article(stubs={})
-    (@mock_article ||= mock_model(Article).as_null_object).tap do |article|
+  
+  def stub_article(stubs={})
+    (@stub_article ||= stub_model(Article).as_null_object).tap do |article|
       article.stub(stubs) unless stubs.empty?
     end
   end
 
-  def mock_comment(stubs={})
-    (@mock_comment ||= mock_model(Comment).as_null_object).tap do |comment|
+  def stub_comment(stubs={})
+    (@stub_comment ||= stub_model(Comment).as_null_object).tap do |comment|
       comment.stub(stubs) unless stubs.empty?
     end
   end
