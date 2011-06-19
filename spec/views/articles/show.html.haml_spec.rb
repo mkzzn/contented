26e1dc777e0cb655fd659d4f44d417abd262570d
Mike Zazaian <mk@mkzzn.org>
2011-06-19 17:55:36 -0400

    refactor view spec for 'articles/show.html.haml'

--- spec/views/articles/show.html.haml_spec.rb
+++ spec/views/articles/show.html.haml_spec.rb
@@ -1,62 +1,41 @@
 require 'spec_helper'
 
 describe "articles/show.html.haml" do
-  def expect_and_render_resources
-    @article.should_receive(:title)
-    @article.should_receive(:body)
-    
-    @comment = mock_model(Comment)
-    @comment.should_receive(:body)
-    
-    assign :comments, [stub_model(Comment), stub_model(Comment)]
-    assign :article, @article
-    assign :comment, @comment
-    render
-  end
-
-  # before(:each) do
-  #   @article_stubs = {
-  #     :category => stub_model(Category),
-  #     :categorized? => true,
-  #     :category_name => "ultracat"
-  #   }
-  # end
-
-  # it "should render a partial for each comment" do
-  #   @article = mock_model(Article, @article_stubs)
-  #   expect_and_render_resources
-  #   view.should render_template(:partial => "comments/_comment", :count => 2)
-  #   rendered.should =~ /ultracat/
-  # end
-
-  # describe "delete link" do
-  #   before(:each) do
-  #     stub_current_ability
-  #     @article = mock_model(Article, @article_stubs)
-  #   end
-
-  #   context "user can delete article" do
-  #     it "should render a link to destroy the article" do
-  #       @ability.can :destroy, @article
-  #       expect_and_render_resources
-  #       rendered.should have_selector(".article input.destroy")
-  #     end
-  #   end
-    
-  #   context "user cannot delete article" do
-  #     it "should not render a link to destroy the article" do
-  #       @ability.cannot :destroy, @article
-  #       expect_and_render_resources
-  #       rendered.should_not have_selector(".article input.destroy")
-  #     end
-  #   end
-  # end
-
   before(:each) do
     @article = stub_article
     assign :article, @article
-    assign :comments, [ stub_comment, stub_comment ] 
+    assign :comments, (1..2).map { stub_comment }
     assign :comment, stub_comment
+  end
+
+  describe "comments partial" do
+    it "should render a partial for each comment" do
+      render
+      view.should render_template(:partial => "comments/_comment", :count => 2)
+    end
+  end
+
+  describe "delete link" do
+    before(:each) do
+      stub_current_ability
+      @article = stub_article
+    end
+
+    context "user is authorized to delete article" do
+      it "should render a link to destroy the article" do
+        @ability.can :destroy, @article
+        render
+        rendered.should have_selector(".article input.destroy")
+      end
+    end
+    
+    context "user is not authorized to delete article" do
+      it "should not render a link to destroy the article" do
+        @ability.cannot :destroy, @article
+        render
+        rendered.should_not have_selector(".article input.destroy")
+      end
+    end
   end
 
   describe "edit link" do
@@ -64,7 +43,7 @@ describe "articles/show.html.haml" do
       stub_current_ability
     end
 
-    context "user can edit article" do
+    context "user is authorized to edit article" do
       it "should render a link to edit the article" do
         @ability.can :edit, Article
         render
@@ -72,7 +51,7 @@ describe "articles/show.html.haml" do
       end
     end
     
-    context "user cannot edit article" do
+    context "user isnot authorized to edit article" do
       it "should not render a link to edit the article" do
         @ability.cannot :edit, Article
         render
@@ -86,7 +65,7 @@ describe "articles/show.html.haml" do
       it "should not render the category link" do
         @article.stub!(:categorized?) { false }
         render
-        rendered.should include("Uncategorized")
+        rendered.should include('<a href="/articles/uncategorized">Uncategorized</a>')
       end
     end
 
@@ -95,7 +74,7 @@ describe "articles/show.html.haml" do
         @category = mock_category :id => 10, :title => "Snake Bait"
         @article.stub!(:category) { @category }
         render
-        rendered.should include("Snake Bait")
+        rendered.should include('<a href="/categories/10">Snake Bait</a>')
       end
     end
   end
