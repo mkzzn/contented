dd8660a123dc42357813305f5bf0556203a388a3
Mike Zazaian <mk@mkzzn.org>
2011-06-20 22:37:37 -0400

    fix some bugs in categories/index.html.haml spec

--- spec/views/categories/index.html.haml_spec.rb
+++ spec/views/categories/index.html.haml_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe "categories/index.html.haml" do
   before(:each) do
-    @category = mock_category
-    @articles = (1..2).map { mock_article }
+    @category = stub_category
+    @articles = (1..2).map { stub_article }
     @category.stub!(:recent_articles) { @articles }
     assign :categories, (1..2).map { @category }
     render
@@ -17,14 +17,14 @@ describe "categories/index.html.haml" do
     view.should render_template(:partial => "articles/_article", :count => 4)
   end
 
-  def mock_article(stubs={})
-    (@mock_article ||= mock_model(Article).as_null_object).tap do |article|
+  def stub_article(stubs={})
+    (@stub_article ||= stub_model(Article).as_null_object).tap do |article|
       article.stub(stubs) unless stubs.empty?
     end
   end
 
-  def mock_category(stubs={})
-    (@mock_category ||= mock_model(Category).as_null_object).tap do |category|
+  def stub_category(stubs={})
+    (@stub_category ||= stub_model(Category).as_null_object).tap do |category|
       category.stub(stubs) unless stubs.empty?
     end
   end
