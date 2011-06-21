232a523d81e085333b357f51de1b4f90a660c625
Mike Zazaian <mk@mkzzn.org>
2011-06-20 21:33:58 -0400

    add scenarios, view specs for seeing edit category link on 'show' category view

--- features/categories/show.feature
+++ features/categories/show.feature
@@ -17,3 +17,19 @@ Feature: Show Category
     When I visit the show page for category "foo"
     Then I should see article "bar" within category "foo"
     And I should see article "baz" within category "foo"
+
+  Scenario: Admin user clicks the edit category link
+    Given I am logged in as a confirmed admin user
+    When I visit the show page for category "foo"
+    And I click the link to edit category "foo"
+    Then I should be on the edit page for category "foo"
+
+  Scenario: Reader cannot see the edit category link
+    Given I am logged in as a confirmed reader user
+    When I visit the show page for category "foo"
+    Then I should not see an edit link for category "foo"
+
+  Scenario: Visitor cannot see the edit category link
+    Given I am not logged in
+    When I visit the show page for category "foo"
+    Then I should not see an edit link for category "foo"
--- spec/views/categories/show.html.haml_spec.rb
+++ spec/views/categories/show.html.haml_spec.rb
@@ -2,12 +2,57 @@ require 'spec_helper'
 
 describe "categories/show.html.haml" do
   before(:each) do
-    articles = [stub_model(Article), stub_model(Article)]
-    assign :category, stub_model(Category, :articles => articles)
-    render
+    @articles = [ stub_article, stub_article ]
+    @category = stub_category
+    @category.stub!(:articles) { @articles }
+    assign :category, @category
+    stub_current_ability
   end
 
   it "should render a partial for every associated article" do
+    render
     view.should render_template(:partial => "articles/_article", :count => 2)
+  end
+
+  describe "edit category link" do
+    context "user is authorized" do
+      before(:each) do
+        @ability.can :edit, Category
+      end
+
+      it "should render an 'edit' link" do
+        render
+        rendered.should have_selector("#category_#{@category.id} a.edit")
+      end
+    end
+
+    context "user is not authorized" do
+      before(:each) do
+        @ability.cannot :edit, Category
+      end
+
+      it "should not render an 'edit' link" do
+        render
+        rendered.should_not have_selector("#category_#{@category.id} a.edit")
+      end
+    end
+  end
+
+  def stub_article(stubs={})
+    (@stub_article ||= stub_model(Article).as_null_object).tap do |article|
+      article.stub(stubs) unless stubs.empty?
+    end
+  end
+
+  def stub_category(stubs={})
+    (@stub_category ||= stub_model(Category).as_null_object).tap do |category|
+      category.stub(stubs) unless stubs.empty?
+    end
+  end
+
+  def stub_current_ability
+    @ability = Object.new
+    @ability.extend(CanCan::Ability)
+    @controller.stub!(:current_ability) { @ability }
   end
 end
