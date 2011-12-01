30358abefd871db22718c49820bbd36bf0bf95f9
Mike Zazaian <mk@mkzzn.org>
2011-12-01 03:15:15 -0500

    remove bogus asset validation

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -2,7 +2,6 @@ class Asset < ActiveRecord::Base
   has_attached_file :data
 
   belongs_to :attachable, :polymorphic => true
-  validate :validate_attachment_size
   
   def url(*args)
     data.url(*args)
