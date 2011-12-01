32e999fa9c8da74c480aaed62d84adf3f2019372
Mike Zazaian <mk@mkzzn.org>
2011-12-01 18:38:54 -0500

    rough implementation of jquery-file-upload

---
+++ public/javascripts/jquery_file_upload.js
@@ -0,0 +1,90 @@
+/*
+ * jQuery File Upload Plugin JS Example 5.1.5
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2010, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://creativecommons.org/licenses/MIT/
+ */
+
+/*jslint nomen: true, unparam: true, regexp: true */
+/*global $, window, document */
+
+$(function () {
+    'use strict';
+
+    // Initialize the jQuery File Upload widget:
+    $('#fileupload').fileupload();
+
+    if (window.location.hostname === 'blueimp.github.com') {
+        // Demo settings:
+        $('#fileupload form').prop(
+            'action',
+            '//jquery-file-upload.appspot.com'
+        );
+        $('#fileupload').fileupload('option', {
+            maxFileSize: 5000000,
+            acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
+        });
+    } else {
+        // Load existing files:
+        $.getJSON($('#fileupload form').prop('action'), function (files) {
+            var fu = $('#fileupload').data('fileupload');
+            fu._adjustMaxNumberOfFiles(-files.length);
+            fu._renderDownload(files)
+                .appendTo($('#fileupload .files'))
+                .fadeIn(function () {
+                    // Fix for IE7 and lower:
+                    $(this).show();
+                });
+        });
+    }
+
+    // Enable iframe cross-domain access via redirect page:
+    var redirectPage = window.location.href.replace(
+        /\/[^\/]*$/,
+        '/result.html?%s'
+    );
+    $('#fileupload').bind('fileuploadsend', function (e, data) {
+        if (data.dataType.substr(0, 6) === 'iframe') {
+            var target = $('<a/>').prop('href', data.url)[0];
+            if (window.location.host !== target.host) {
+                data.formData.push({
+                    name: 'redirect',
+                    value: redirectPage
+                });
+            }
+        }
+    });
+
+    // Open download dialogs via iframes,
+    // to prevent aborting current uploads:
+    $('#fileupload .files').delegate(
+        'a:not([rel^=gallery])',
+        'click',
+        function (e) {
+            e.preventDefault();
+            $('<iframe style="display:none;"></iframe>')
+                .prop('src', this.href)
+                .appendTo(document.body);
+        }
+    );
+
+    // Initialize the Image Gallery widget:
+    $('#fileupload .files').imagegallery();
+
+    // Initialize the theme switcher:
+    $('#theme-switcher').change(function () {
+        var theme = $('#theme');
+        theme.prop(
+            'href',
+            theme.prop('href').replace(
+                /[\w\-]+\/jquery-ui.css/,
+                $(this).val() + '/jquery-ui.css'
+            )
+        );
+    });
+
+});
