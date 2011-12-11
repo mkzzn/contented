d4cfcffe7902ea7f35421f9cec6204be0b153cdc
Mike Zazaian <mk@mkzzn.org>
2011-12-11 17:22:32 -0500

    add steps for featured image

---
+++ features/step_definitions/articles/featured_article_steps.rb
@@ -0,0 +1,15 @@
+Given /^(article "\w+") with attachment "([^"]*)"$/ do |article, image_path|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I select "([^"]*)" as the featured image$/ do |image_path|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I select no featured image$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^(article "\w+") should not have a featured image$/ do |article|
+  pending # express the regexp above with the code you wish you had
+end
