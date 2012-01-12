d6d3dea87837bc9a873b2cd13f9a94a4b5e04f47
Mike Zazaian <mk@mkzzn.org>
2012-01-12 05:56:03 -0500

    more style on article byline

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,9 @@
 module CategoriesHelper
+  def category_link(article)
+    if article.categorized?
+      link_to article.category_name, article.category
+    else
+      link_to "Uncategorized", uncategorized_articles_path
+    end
+  end
 end
